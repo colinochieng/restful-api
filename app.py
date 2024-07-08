1a92874a1bf3fc14e2413de255b7b424d9f97b67
colinochieng <colinokumu89@gmail.com>
2024-07-08 21:36:12 +0300

    Update on the Database

--- app.py
+++ app.py
@@ -15,10 +15,12 @@ from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
 from resources.user import blp as UserBlueprint
+from dotenv import load_dotenv
 
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
 
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Stores REST API"
